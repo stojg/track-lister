e28e745af57dcf8dc529ebe699b564187256a392
Stig Lindqvist <me@stig.nz>
2020-04-23 16:09:06 +1200

    styles and templates and stuff

--- main.go
+++ main.go
@@ -5,10 +5,12 @@
 //       - Use "http://localhost:8080/callback" as the redirect URI
 //  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
 //  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
+//  4. Override the CALLBACK_URL environment variable if not using localhost callback
 package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -37,76 +39,99 @@ func main() {
 		auth = spotify.NewAuthenticator(defaultRedirectURI, spotify.ScopeUserReadPrivate)
 	}
 
-	// first start an HTTP server
-	http.HandleFunc("/callback", completeAuth)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html; charset=utf-8")
-
-		sp_token, err := r.Cookie("sp_token")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if sp_token == nil || sp_token.Value == "" {
-			url := auth.AuthURL(state)
-			http.Redirect(w, r, url, http.StatusFound)
-			return
-		}
-
-		tok := &oauth2.Token{
-			AccessToken: sp_token.Value,
-			TokenType:   "bearer",
-		}
-
-		fmt.Fprintln(w, "<p>Enter a spotify playlist URI like 'spotify:playlist:6fCOzHcpq7P25OZC8Mikxr'</p>")
-		playlist := spotify.ID("")
-		playlistID := r.FormValue("playlist")
-		if playlistID != "" {
-			parts := strings.Split(playlistID, ":")
-			if len(parts) != 3 {
-				fmt.Fprintln(w, "<p style='font-weight:bold;color:#f44;'>Please use a Spotify URI like 'spotify:playlist:6fCOzHcpq7P25OZC8Mikxr'</p>")
-			}
-			playlist = spotify.ID(parts[2])
-		}
-
-		formSize := 40
-		if playlistID != "" && len(playlistID) > 40 {
-			formSize = len(playlistID)
-		}
-		fmt.Fprintf(w, "<p><form><input name=\"playlist\" value=\"%s\" size=%d /><input type=\"submit\"></form></p>", playlistID, formSize)
-		if playlist == "" {
-			return
-		}
-
-		log.Printf("Recieved request for playlist id %s", playlistID)
-
-		client := auth.NewClient(tok)
-		pl, err := client.GetPlaylist(playlist)
-		if err != nil {
-			fmt.Fprintf(w, "<p style='font-weight:bold;color:#f44;'>Error: '%s'</p>", err)
-			log.Printf("Error: '%s'", err)
-			return
-		}
-
-		log.Printf("Found %d tunes\n", len(pl.Tracks.Tracks))
-		for _, track := range pl.Tracks.Tracks {
-			artist := track.Track.Artists[0].Name
-			trackName := track.Track.Name
-			view := fmt.Sprintf("%s - %s", artist, trackName)
-			query := strings.Replace(view, " ", "+", -1)
-			fmt.Fprintf(w, "%s <a href=\"https://www.beatport.com/search/tracks?q=%s\">beatport</a><br>", view, query)
-		}
-	})
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/callback", authHandler)
+	http.HandleFunc("/search", searchHandler)
 
 	go http.ListenAndServe(":8080", nil)
 
-	log.Println("Open a browser with http://localhost:8080/")
+	log.Println("listening on :8080")
 
 	<-ch
 }
 
-func completeAuth(w http.ResponseWriter, r *http.Request) {
+type pageData struct {
+	Title   string
+	Warning string
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		writeTemplate(w, http.StatusNotFound, "not_found.html", pageData{
+			Title: "404 - not found",
+		})
+		return
+	}
+
+	writeTemplate(w, http.StatusOK, "index.html", pageData{
+		Title: "Track lister",
+	})
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	oauthToken, err := r.Cookie("sp_token")
+	if err != nil && err != http.ErrNoCookie {
+		serverErrorHandler(w, err)
+		return
+	}
+
+	if oauthToken == nil || oauthToken.Value == "" {
+		url := auth.AuthURL(state)
+		http.Redirect(w, r, url, http.StatusFound)
+		return
+	}
+
+	tok := &oauth2.Token{
+		AccessToken: oauthToken.Value,
+		TokenType:   "bearer",
+	}
+
+	type searchData struct {
+		pageData
+		PlaylistID string
+		Tracks     []spotify.FullTrack
+	}
+
+	playlistID := r.FormValue("playlist")
+	playlistID = strings.TrimSpace(playlistID)
+	pd := searchData{
+		pageData:   pageData{Title: "Search"},
+		PlaylistID: playlistID,
+	}
+
+	playlist := spotify.ID("")
+	if playlistID != "" {
+		parts := strings.Split(playlistID, ":")
+		if len(parts) != 3 {
+			pd.Warning = "That did not looks like a valid search term"
+		} else {
+			playlist = spotify.ID(parts[2])
+		}
+	}
+
+	if playlist == "" {
+		writeTemplate(w, 401, "search.html", pd)
+		return
+	}
+
+	log.Printf("Received search request for playlist id %s", playlistID)
+
+	client := auth.NewClient(tok)
+	pl, err := client.GetPlaylist(playlist)
+	if err != nil {
+		pd.Warning = err.Error()
+	} else {
+		log.Printf("Found %d tunes\n", len(pl.Tracks.Tracks))
+
+		for _, track := range pl.Tracks.Tracks {
+			pd.Tracks = append(pd.Tracks, track.Track)
+		}
+	}
+
+	writeTemplate(w, 200, "search.html", pd)
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
 	tok, err := auth.Token(state, r)
 	if err != nil {
 		http.Error(w, "Couldn't get token", http.StatusForbidden)
@@ -122,5 +147,42 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 	cookie := http.Cookie{Name: "sp_token", Value: tok.AccessToken, Expires: tok.Expiry}
 	http.SetCookie(w, &cookie)
 
-	http.Redirect(w, r, "/", http.StatusFound)
+	http.Redirect(w, r, "/search", http.StatusFound)
+}
+
+func writeTemplate(w http.ResponseWriter, statusCode int, tmplFile string, data interface{}) {
+	t, err := template.ParseGlob("./templates/*")
+	if err != nil {
+		serverErrorHandler(w, err)
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	err = t.ExecuteTemplate(w, tmplFile, data)
+	if err != nil {
+		serverErrorHandler(w, err)
+		return
+	}
+}
+
+func serverErrorHandler(w http.ResponseWriter, err error) {
+	t, tErr := template.ParseGlob("./templates/*")
+	if tErr != nil {
+		fmt.Fprintf(w, "<p>500 Server error</p><p>%s</p>", err)
+		log.Printf("server error: %s\n", err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(500)
+	sErr := t.ExecuteTemplate(w, "error.html", pageData{
+		Title:   "Server error",
+		Warning: err.Error(),
+	})
+	if sErr != nil {
+		fmt.Fprintf(w, "<p>500 Server error</p><p>%s</p>", err)
+		log.Printf("server error: %s\n", err)
+		return
+	}
 }
